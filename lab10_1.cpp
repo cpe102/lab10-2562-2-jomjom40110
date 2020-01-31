7e6e7e542f307e65494cd000f3f07b2ccd598793
jomjom40110 <jomjom40110@gmail.com>
2020-01-31 17:01:42 +0700

    lab10

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,20 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<string>
+using namespace std;
+
+int main(){
+    srand(time(0));
+	string grade[9]={"A","B+","B","C+","C","D+","D","F","W"};
+	int x = rand()%9;
+    cout << "Press Enter 3 times to reveal your future."<<endl;
+    cin.get();
+    cin.get();
+    cin.get();
+    cout << "You will get "<< grade[x] << " in this 261102";
+    return 0;
+}
